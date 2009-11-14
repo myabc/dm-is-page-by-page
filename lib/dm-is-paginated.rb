ece61cc17bf2e65140fd5b6147fc454a68b9735e
Alex Coles <alex@alexcolesportfolio.com>
2009-11-14 17:30:44 +0100

    Don't explicitly require RubyGems
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- lib/dm-is-paginated.rb
+++ lib/dm-is-paginated.rb
@@ -1,9 +1,7 @@
 # Needed to import datamapper and other gems
-require 'rubygems'
 require 'pathname'
 
 # Add all external dependencies for the plugin here
-gem 'dm-core', '>=0.9.4'
 require 'dm-core'
 
 # Require plugin-files
